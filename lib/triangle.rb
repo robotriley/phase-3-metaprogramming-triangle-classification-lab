64d570c156b73e0a7b788ea2748d6d9b81ebdede
riley soloner <rileysoloner@gmail.com>
2022-08-12 14:27:45 -0400

    commit tree 91b5a1e19f302e717403b0d4dd1169f0c00e622e

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
+  attr_accessor :length1, :length2, :length3
+
+def initialize(length1, length2, length3)
+  @length1 = length1
+  @length2 = length2
+  @length3 = length3
+end
+
+def kind
+  if length1 == length2 && length1 == length3 && length2 == length3
+  :equilateral
+  elsif length1 == length2 || length1 == length3 || length2 == length3
+    :isosceles
+  else
+    :scalene
+  elsif length1 == nil || length2 == nil || length3 == nil
+    raise TriangleError
+  elsif
+    length1 < 0 || length2 < 0 || length3 < 0
+    raise TriangleError
+  elsif
+    length1 + length2 < length3 || length1 + length3 < length2 || length2 + length3 < length1
+    raise TriangleError
+  end
+end
+
+class TriangleError < StandardError
+
+  end
 end
